360cefde0aeef95856ea6b77a7b36b4fd9c9b488
jasonmziegler <jason@insightfulautomation.com>
2018-10-30 22:40:57 -0500

    Add ajax translate

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import (render_template, flash, redirect, url_for, request,
+                   g, jsonify)
 from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import (LoginForm,
@@ -8,9 +9,12 @@ from app.forms import (LoginForm,
                        ResetPasswordRequestForm,
                        ResetPasswordForm)
 from flask_login import current_user, login_user, logout_user, login_required
+from flask_babel import _, get_locale
 from app.models import User, Post
 from app.email import send_password_reset_email
+from app.translate import translate
 from datetime import datetime
+from guess_language import guess_language
 
 
 @app.before_request
@@ -18,6 +22,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -26,10 +31,16 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+
+        post = Post(body=form.post.data,
+                    author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
-        flash('Your post is now live!')
+        flash(_('Your post is now live!'))
         return redirect(url_for('index'))
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
@@ -46,15 +57,15 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        flash('You are currently logged in as {}'.format(
-              current_user.username
-              ))
+        flash(_('You are currently logged in as %(username)',
+              username=current_user.username
+                ))
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash(_('Invalid username or password'))
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
@@ -80,7 +91,7 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registered user!')
+        flash(_('Congratulations, you are now a registered user!'))
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
@@ -108,7 +119,7 @@ def edit_profile():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
-        flash('Your changes have been saved.')
+        flash(_('Your changes have been saved.'))
         return redirect(url_for('edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
@@ -122,14 +133,14 @@ def edit_profile():
 def follow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User {} not found.'.format(username))
+        flash(_('User %(username) not found.', username=username))
         return redirect(url_for('index'))
     if user == current_user:
-        flash('You cannot follow yourself!')
+        flash(_('You cannot follow yourself!'))
         return redirect(url_for('user', username=username))
     current_user.follow(user)
     db.session.commit()
-    flash('You are now following {}!'.format(username))
+    flash(_('You are now following %(username)!', username=username))
     return redirect(url_for('user', username=username))
 
 
@@ -138,14 +149,14 @@ def follow(username):
 def unfollow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User {} not found.'.format(username))
+        flash(_('User %(username) not found.', username=username))
         return redirect(url_for('index'))
     if user == current_user:
-        flash('You cannot unfollow yourself!')
+        flash(_('You cannot unfollow yourself!'))
         return redirect(url_for('user', username=username))
     current_user.unfollow(user)
     db.session.commit()
-    flash('You are no longer following {}'.format(username))
+    flash(_('You are no longer following %(username)', username=username))
     return redirect(url_for('user', username=username))
 
 
@@ -173,7 +184,9 @@ def reset_password_request():
         user = User.query.filter_by(email=form.email.data).first()
         if user:
             send_password_reset_email(user)
-        flash('Check your email for the instructions to reset your password')
+        flash(_(
+            'Check your email for the instructions to reset your password')
+        )
         return(redirect(url_for('login')))
     return render_template('reset_password_request.html',
                            title='Reset Password', form=form)
@@ -191,6 +204,14 @@ def reset_password(token):
         if form.validate_on_submit():
             user.set_password(form.password.data)
             db.session.commit()
-            flash('Your password has been reset.')
+            flash(_('Your password has been reset.'))
             return redirect(url_for('login'))
         return render_template('reset_password.html', form=form)
+
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
--- config.py
+++ config.py
@@ -16,3 +16,5 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['python@minewhileyousleep.com']
     POSTS_PER_PAGE = 25
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
